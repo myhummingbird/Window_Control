cfae00bdc5d25c35a7abeecfc3ab29d35e7e3b73
Jakree Winyan <jakree@gmail.com>
2018-11-05 23:23:26 +0700

    Update: read hc165 and write to hc595

--- Code/main.c
+++ Code/main.c
@@ -35,29 +35,15 @@ int main(void)
 	hc165_init();
 	hc595_init();
 	
-	output[0] = 1;
-	
-	hc595_write(output, WINDOW_NO);
-	
 	while (1)
 	{
 		//PORTC = adc_start_conversion(0);
 		
 		//get_window(WINDOW_NO, adc, input);
 		
-		//hc595_write(adc, WINDOW_NO);
-		
-		_delay_ms(500);
-		hc595_write(output, WINDOW_NO);
-		output[i] *= 2;
-		if (output[i] == 0)
-		{
-			i++;
-			if (i >=  WINDOW_NO)
-				i = 0;
-			output[i] = 1;
-		}
-		
+		hc165_read(input, WINDOW_NO);
+		hc595_write(input, WINDOW_NO);
+		_delay_ms(100);
 	}
 
 	return 0;
